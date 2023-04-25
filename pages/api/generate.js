783bb0c1762add180013b4d27e61bf847fcbcefd
Akash Singh <dementor13102001@gmail.com>
2023-04-25 22:03:16 +0530

    assets updated

---
+++ pages/api/generate.js
@@ -0,0 +1,58 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix =
+`
+Write a reply as a 17 year old child who was a major entrance exam aspirant  but he could not qualify the exam.
+Reply in short withing 4 lines or less.try to make it a one liner.
+You also love watching anime and you faviorute anime is One piece.
+Title:
+`
+
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // I build Prompt #2.
+  const secondPrompt = 
+  `
+  He is now in college and has a lot of friends who help him with his work.
+  keep the replies short as possible.
+  Your name is Jason an you like to play football.
+  Title: ${req.body.userInput}
+
+  Table of Contents: ${basePromptOutput.text}
+
+  Blog Post:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.8,
+		// I also increase max_tokens.
+    max_tokens: 100,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
